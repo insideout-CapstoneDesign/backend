bb764a5509766903d5621912de88d8088b26f281
thisminji <thisminji@dgu.ac.kr>
2026-04-29 20:29:16 +0900

    chore: user 예제 예외 코드 정리 및 미사용 Example 파일 제거

--- src/main/java/com/insideout/backend/domain/user/exception/ExampleException.java
+++
@@ -1,9 +0,0 @@
-package com.insideout.backend.domain.user.exception;
-
-import com.insideout.backend.global.apiPayload.exception.ProjectException;
-
-public class ExampleException extends ProjectException {
-    public ExampleException(ExampleErrorCode errorCode) {
-        super(errorCode);
-    }
-}

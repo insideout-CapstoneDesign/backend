78d246f0e9ca2e846fef4427469f3332e1e1a20a
sekong11 <sekong1108@gmail>
2026-05-05 00:32:23 +0900

    refactor: 공통 응답 반환 구조 변경 (#19)

--- src/main/java/com/insideout/backend/global/apiPayload/ApiResponse.java
+++ src/main/java/com/insideout/backend/global/apiPayload/ApiResponse.java
@@ -38,24 +38,9 @@ public class ApiResponse<T> {
         this.result = result;
     }
 
-    // [성공] 가장 많이 쓰는 200 OK (결과 데이터 있음)
-    public static <T> ResponseEntity<ApiResponse<T>> OK(T result) {
-        return of(GeneralSuccessCode.OK, result);
-    }
-
-    // [성공] 가장 많이 쓰는 200 OK (결과 데이터 없음)
-    public static <T> ResponseEntity<ApiResponse<T>> OK() {
-        return of(GeneralSuccessCode.OK, null);
-    }
-
-    // [성공] 201, 202 등 커스텀 성공 상태가 필요할 때
-    public static <T> ResponseEntity<ApiResponse<T>> of(BaseSuccessCode code, T result) {
-        if (code.getStatus() == HttpStatus.NO_CONTENT) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity
-                .status(code.getStatus())
-                .body(onSuccessBody(code, result));
+    // [성공]
+    public static <T> ApiResponse<T> success(BaseSuccessCode code, T result) {
+        return new ApiResponse<>(true, code.getCode(), code.getMessage(), result);
     }
 
     // [실패] 에러 핸들러에서 사용 (데이터 없는 일반 에러)
@@ -70,10 +55,6 @@ public class ApiResponse<T> {
         return ResponseEntity
                 .status(code.getStatus())
                 .body(onFailureBody(code, result));
-    }
-
-    private static <T> ApiResponse<T> onSuccessBody(BaseSuccessCode code, T result) {
-        return new ApiResponse<>(true, code.getCode(), code.getMessage(), result);
     }
 
     private static <T> ApiResponse<T> onFailureBody(BaseErrorCode code, T result) {

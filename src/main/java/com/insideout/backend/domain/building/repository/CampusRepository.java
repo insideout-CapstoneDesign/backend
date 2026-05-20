3efd9504811a8da222c79668c87fb5bb2f25dd4d
sekong11 <sekong1108@gmail>
2026-05-21 03:39:39 +0900

    feat: repository 메서드 추가 (#44)

--- src/main/java/com/insideout/backend/domain/building/repository/CampusRepository.java
+++ src/main/java/com/insideout/backend/domain/building/repository/CampusRepository.java
@@ -4,8 +4,10 @@ import com.insideout.backend.domain.building.entity.Campus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CampusRepository extends JpaRepository<Campus, UUID> {
+    Optional<Campus> findByIdAndTenant_Id(UUID id, UUID tenantId);
 }

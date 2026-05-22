6832c64d9a8a6c5179dcfb72541cbf3cac94a172
sekong11 <sekong1108@gmail>
2026-05-22 15:58:19 +0900

    fix: 코드래빗 피드백 반영 (#44)

--- src/main/java/com/insideout/backend/domain/building/dto/request/BuildingCreateRequestDTO.java
+++ src/main/java/com/insideout/backend/domain/building/dto/request/BuildingCreateRequestDTO.java
@@ -3,6 +3,7 @@ package com.insideout.backend.domain.building.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,7 +16,7 @@ public record BuildingCreateRequestDTO(
         String externalApiId,
         String longitude,
         String latitude,
-        @Valid List<FloorCreateRequest> floors
+        List<@NotNull @Valid FloorCreateRequest> floors
 ) {
     public record FloorCreateRequest(
             int level,
